ce2d4b509a3f3d1437bd36a3241e74dbc400df81
Aniket Shinde <aniket.shinde@remiges.tech>
2025-05-25 10:59:29 +0530

    feat (remiges-asset) - update title and loading text

--- remiges-asset/serversage.sh
+++ remiges-asset/serversage.sh
@@ -10,3 +10,17 @@ cp -f img/apple-touch-icon.png /usr/share/grafana/public/img/apple-touch-icon.pn
 cp -f img/grafana_icon.svg /usr/share/grafana/public/img/grafana_icon.svg
 cp img/g8_login_dark.svg /usr/share/grafana/public/img/g8_login_dark.svg
 cp img/g8_login_light.svg /usr/share/grafana/public/img/g8_login_light.svg
+
+
+##################################################################
+# HTML & JS CUSTOMIZATION - Update titles, menus, and branding
+##################################################################
+# Update title and loading text in index.html
+sed -i 's|<title>\[\[.AppTitle\]\]</title>|<title>ServerSage</title>|g' /usr/share/grafana/public/views/index.html && \
+sed -i 's|Loading Grafana|Loading ServerSage|g' /usr/share/grafana/public/views/index.html
+
+find /usr/share/grafana/public/build/ -name *.js -exec sed -i 's|{target:"_blank",id:"version",text:`${e.edition}${s}`,url:t.licenseUrl}||g' {} \;
+
+find /usr/share/grafana/public/build/ -name *.js -exec sed -i 's|{target:"_blank",id:"version",text:`v${e.version} (${e.commit})`,url:i?"https://github.com/grafana/grafana/blob/main/CHANGELOG.md":void 0}||g' {} \;
+
+find /usr/share/grafana/public/build/ -name *.js -exec sed -i 's|{target:"_blank",id:"updateVersion",text:"New version available!",icon:"download-alt",url:"https://grafana.com/grafana/download?utm_source=grafana_footer"}||g' {} \;
