1ab8d55178a20708b51a738f524676b1dfa140d5
Aniket Shinde <aniket.shinde@remiges.tech>
2025-06-09 15:42:12 +0530

    chore : crd's updated for prometheus operator

--- application-files/kubernetes-deployements/high-availibility/prometheus-operator-no-helm/crd/download-crd.sh
+++ application-files/kubernetes-deployements/high-availibility/prometheus-operator-no-helm/crd/download-crd.sh
@@ -2,12 +2,15 @@
 # Fix Prometheus Operator setup
 
 echo "Step 1: Downloading Prometheus CRDs directly..."
-curl -o prometheuses.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml
+curl -o alertmanagerconfigs.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml
 curl -o alertmanagers.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml
-curl -o servicemonitors.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
 curl -o podmonitors.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
-curl -o prometheusrules.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml
-curl -o thanosrulers.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml
 curl -o probes.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_probes.yaml
+curl -o prometheusagents.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_prometheusagents.yaml
+curl -o prometheuses.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml
+curl -o prometheusrules.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml
+curl -o scrapeconfigs.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_scrapeconfigs.yaml
+curl -o servicemonitors.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
+curl -o thanosrulers.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.0/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml
 
 echo "Done! Downloading crd's ..... "
